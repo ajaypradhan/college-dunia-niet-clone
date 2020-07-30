05c682faea6ed00a0def36ed32797e961424ba7a
ajaypradhan <pradhanajay14@gmail.com>
2020-07-30 15:56:42 +0530

    NIET College Dunia Clone

---
+++ main.js
@@ -0,0 +1,10 @@
+function myFunction1() {
+    var x = document.getElementById("bdata1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
